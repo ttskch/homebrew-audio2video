6604da54bf7f253c719478d8f1e887c14e6e42ce
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-02-17 16:04:39 +0900

    version 0.0.1

--- audio2video.rb
+++ audio2video.rb
@@ -1,37 +1,31 @@
-# Documentation: http://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require File.expand_path("../../../Homebrew/homebrew-php/language/php", __FILE__)
+require File.expand_path("../../../Homebrew/homebrew-php/Requirements/php-meta-requirement", __FILE__)
 
 class Audio2video < Formula
-  desc ""
-  homepage ""
+  include Language::PHP::Composer
+
+  desc "A CLI tool to convert an audio file to a simple video file."
+  homepage "https://github.com/ttskch/audio2video-cli"
   url "https://github.com/ttskch/audio2video-cli/archive/0.0.1.tar.gz"
   sha256 "a5f9e8b4255ef2e5ab085af0e92964acc4365c75a6485566608a5d42956efdc8"
 
-  # depends_on "cmake" => :build
+  depends_on PhpMetaRequirement
+  depends_on "ffmpeg" => "with-tools"
+  depends_on "imagemagick"
+  depends_on "php53-imagick" if Formula["php53"].linked_keg.exist?
+  depends_on "php54-imagick" if Formula["php54"].linked_keg.exist?
+  depends_on "php55-imagick" if Formula["php55"].linked_keg.exist?
+  depends_on "php56-imagick" if Formula["php56"].linked_keg.exist?
+  depends_on "php70-imagick" if Formula["php70"].linked_keg.exist?
+  depends_on "php71-imagick" if Formula["php71"].linked_keg.exist?
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    composer_install
+    bin.install ["audio2video", "vendor"]
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test audio2video`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/audio2video"
+    system "#{bin}/audio2video", "conv", "-h"
   end
 end
